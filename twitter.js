7676518123f3160c4f5d9fc6b7aaf664734d601d
Timur B <digitalNomadKg@users.noreply.github.com>
2020-01-25 17:28:33 +0200

    minor change

--- twitter.js
+++ twitter.js
@@ -3,4 +3,7 @@ var tweet = prompt("Compose your tweet");
 var tweetUnder140 = tweet.slice(0,140);
 alert(tweetUnder140);
 
+
+//alert (prompt("Compose your tweet:").slice(0,140)); second option
+
 // alert("You have written " + tweet.length + " characters, you have " (140 - tweet.length) + " characters reamining");
