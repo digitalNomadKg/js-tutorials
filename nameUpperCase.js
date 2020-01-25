1c47c1ee2dbf5b8e74e9184ab0382d1a5f33d66d
Timur B <digitalNomadKg@users.noreply.github.com>
2020-01-25 18:15:16 +0200

    nameToLowerCasae

--- nameUpperCase.js
+++ nameUpperCase.js
@@ -7,5 +7,6 @@ var name = prompt("What is your name?");
 var firstChar = name.slice(0,1);
 var upperCaseFirstChar= firstChar.toUpperCase();
 var restOfName = name.slice(1,name.length);
-var capitalisedName = upperCaseFirstChar + restOfName;
+var lowerCaseAll = restOfName.toLowerCase();
+var capitalisedName = upperCaseFirstChar + lowerCaseAll;
 alert ("Hello, " + capitalisedName);
