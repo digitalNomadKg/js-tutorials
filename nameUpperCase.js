c0f9a4bf18e4558c99f64763748914fce6e9aed9
Timur B <digitalNomadKg@users.noreply.github.com>
2020-01-25 18:05:21 +0200

    nameUpperCase

---
+++ nameUpperCase.js
@@ -0,0 +1,11 @@
+var name = "Tim Kirves";
+name = name.toUpperCase();
+name = name.toLowerCase();
+
+
+var name = prompt("What is your name?");
+var firstChar = name.slice(0,1);
+var upperCaseFirstChar= firstChar.toUpperCase();
+var restOfName = name.slice(1,name.length);
+var capitalisedName = upperCaseFirstChar + restOfName;
+alert ("Hello, " + capitalisedName);
