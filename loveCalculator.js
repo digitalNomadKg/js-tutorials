d3d81d44ca2f9e2d7badbb814b968f581a2f2cb7
Timur B <digitalNomadKg@users.noreply.github.com>
2020-01-25 20:00:54 +0200

    loveCalculator initial commit

---
+++ loveCalculator.js
@@ -0,0 +1,33 @@
+//random number generator
+
+// var n = Math.random();
+var n = Math.random();
+n= n *6;
+n = Math.floor(n) + 1; //pseudo number generator
+console.log(n);
+
+
+
+prompt ("What it is your name?")
+prompt ("What it is their name?")
+
+var loveScore  =Math.random() * 100;
+loveScore=Math.floor(loveScore)+1;
+
+if(loveScore > 70){
+  alert("Your love score is " + loveScore + "%" + "you love each other like me and ma babe");
+
+}else{
+  alert("Your love score is " + loveScore + "%" );
+}
+
+// comparators
+/* 
+=== is equal to
+!== is  not equal to
+> greater than
+< less than
+>= is greater or equal to
+<= is less than or equal to
+*/
+
