440be65b0a58e4ebd4849357e114e64ca5a66459
Timur B <digitalNomadKg@users.noreply.github.com>
2020-01-26 00:01:18 +0200

    loveCalculator minor changes, comparators added

--- loveCalculator.js
+++ loveCalculator.js
@@ -2,32 +2,47 @@
 
 // var n = Math.random();
 var n = Math.random();
-n= n *6;
+n = n * 6;
 n = Math.floor(n) + 1; //pseudo number generator
 console.log(n);
 
 
+// LoveCalculator
 
-prompt ("What it is your name?")
-prompt ("What it is their name?")
 
-var loveScore  =Math.random() * 100;
-loveScore=Math.floor(loveScore)+1;
+prompt("What it is your name?")
+prompt("What it is their name?")
 
-if(loveScore > 70){
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+if (loveScore > 70) {
   alert("Your love score is " + loveScore + "%" + "you love each other like me and ma babe");
+}
+if (loveScore > 30 && loveScore <= 70) {
 
-}else{
-  alert("Your love score is " + loveScore + "%" );
+  alert("Your loveScore is " + loveScore + "%")
+}
+if (loveScore <= 30) {
+  alert("Your loveScore is " + loveScore + "%" + "you go together like Romeo and Juliet");
+
+} else {
+  alert("Your love score is " + loveScore + "%");
 }
 
 // comparators
-/* 
+/*
 === is equal to
 !== is  not equal to
 > greater than
 < less than
 >= is greater or equal to
 <= is less than or equal to
+
+
+&& AND
+|| or
+! not
+
 */
 
