6ea65eb52826996d720e94d9b94a79e62f47e7e8
Timur B <digitalNomadKg@users.noreply.github.com>
2020-01-27 23:18:30 +0200

    guestList task added

--- namesList.js
+++ namesList.js
@@ -16,3 +16,15 @@ var guestList = ["Tim", "Angeala", "Kristi", "Lara", "Jason", "Mark"];
 console.log(guestList);
 console.log(guestList.length);
 console.log(guestList[4]);
+
+
+// prompt Welcome, what is your name?
+// check if name is inside array, than welcome the user, if not than sorry you are not inside the list
+
+var guestName= prompt("Hello, please enter your name");
+var guestList = ["Tim", "Angela", "Kristi", "Lara", "Jason", "Mark"];
+if(guestList.includes(guestName)){
+  alert("Welcome to the candy shop " + guestName);
+}else{
+  alert("get the fuck of here " + guestName);
+}
