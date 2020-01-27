53d695d4c6dd74c9bdae40209366494d026bcfb5
Timur B <digitalNomadKg@users.noreply.github.com>
2020-01-28 00:32:50 +0200

    fizzBuzz minor changes in code

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -19,9 +19,16 @@ var output = [];
 var count = 0;
 
 function fizzBuzz() {
-  output.push(count);
-  count++;  
-
+  if (count % 3 === 0 && count % 5 === 0) {
+    output.push("FizzBuzz");
+  } else if (count % 3 === 0) {
+    output.push("Fizz");
+  } else if (count % 5 === 0) {
+    output.push("Buzz")
+  } else {
+    output.push(count);
+  }
+  count++;
   console.log(output);
 }
 
