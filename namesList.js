aa49e266cf9dd63db0a1dec0d07ff8275fda629d
Timur B <digitalNomadKg@users.noreply.github.com>
2020-01-27 23:01:54 +0200

    namesList initial commit

---
+++ namesList.js
@@ -0,0 +1,18 @@
+/*
+Tim
+Angela
+Mark
+Bob
+Jim
+Lando
+Retko
+Amelia
+Ruth
+Victoria
+*/
+
+
+var guestList = ["Tim", "Angeala", "Kristi", "Lara", "Jason", "Mark"];
+console.log(guestList);
+console.log(guestList.length);
+console.log(guestList[4]);
