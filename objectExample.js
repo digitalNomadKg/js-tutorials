8179632d0a100b00d5568a406c7096e7f9579e26
Tim Kirves <tim.kirves@gmail.com>
2021-03-07 23:36:19 +0200

    finished drum example, call back function example added

--- objectExample.js
+++ objectExample.js
@@ -3,19 +3,40 @@ var clientInfo = {
     lastName: "Kirves",
     id: 3551244612223,
     permit: true,
-    age: 33
+    age: 33,
 }
 clientInfo;
 
 //constructor function
 
-function Client(firstName, lastName, age, experience, id) {
-    this.firstName.uppeCase = firstName;
+function Client(firstName, lastName, age, experience, id, moveSuitcase) {
+    this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
     this.experience = experience;
     this.id = id;
+    this.moveSuitcase = function() {
+        alert("May I take your luggage, please?");
+        pickUpSuitcase();
+        moveBy();
+    }
 }
 
 var client1 = new Client("Tim", "Kirves", 25, 20, 3854777866844);
-let client2 = new Client("Angela", "Yujin", 55, 33, 462458774556);
+var client2 = new Client("Angela", "Yujin", 55, 33, 462458774556);
+var clien3 = new Client("Miku Chan", "Kirves", 25, 35, 466568663);
+
+//----------------------------------------------------------------------------------
+// House keeper orders
+function HouseKeeper(name, yearsExpreince, cleaningArea, cleaning) {
+    this.name = name;
+    this.yearsExpreince = yearsExpreince;
+    this.cleaningArea = cleaningArea;
+    this.cleaning = function() {
+        alert("Cleaning in progress");
+    }
+}
+
+let houseKeeper1 = new HouseKeeper("GulChapChap", 35, 25457);
+
+houseKeeper1.cleaning();
