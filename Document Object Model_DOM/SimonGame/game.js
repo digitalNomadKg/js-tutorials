5b47bc60e9e9897e8a579f6a7ca4f26b0f6d853c
Tim Kirves <tim.kirves@gmail.com>
2021-03-19 22:51:04 +0200

    added some examplaes

--- Document Object Model_DOM/SimonGame/game.js
+++ Document Object Model_DOM/SimonGame/game.js
@@ -1,15 +1,38 @@
-// $(document).ready(function(){
-//   alert("Jquery is working now");
-// })
 
 let gamePattern = [];
+gamePattern= gamePattern.push();
+
 let buttonColours = ["red", "blue", "green", "yellow"];
-let randomChosenColour = [nextSequence(buttonColours)];
+let randomChosenColour = nextSequence(buttonColours);
 
 function nextSequence() {
-  let randomNumber = Math.floor((Math.random() * 3) + 1);
-}
+  randomNumber = (Math.floor(Math.random()*3) + 1);
+  console.log(randomNumber);
+};
+nextSequence();
 
-gamePattern.push(randomChosenColour);
 
-// $("button").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+$("#green").click(function(){
+  var addAudio = new Audio("sounds/green.mp3");
+  addAudio.play();
+});
+
+$("#red").click(function(){
+  var addAudio = new Audio("sounds/red.mp3");
+  addAudio.play();
+});
+
+$("#blue").click(function(){
+  var addAudio = new Audio("sounds/blue.mp3");
+  addAudio.play();
+});
+
+$("#yellow").click(function(){
+  var addAudio = new Audio("sounds/yellow.mp3");
+  addAudio.play();
+});
+
+
+
+
+// // $("button").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
