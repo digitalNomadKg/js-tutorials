9ae342deb3ce003ee093afd49d4c4a0c2b2e4e6e
Tim Kirves <tim.kirves@gmail.com>
2021-03-24 23:23:26 +0200

    simon game example

--- Document Object Model_DOM/SimonGame/game.js
+++ Document Object Model_DOM/SimonGame/game.js
@@ -1,38 +1,51 @@
 
-let gamePattern = [];
-gamePattern= gamePattern.push();
 
 let buttonColours = ["red", "blue", "green", "yellow"];
-let randomChosenColour = nextSequence(buttonColours);
+
+let gamePattern = [];
+
+let userClickedPattern = [];
+
+$(".btn").click(function () {
+  let userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+});
 
 function nextSequence() {
-  randomNumber = (Math.floor(Math.random()*3) + 1);
-  console.log(randomNumber);
-};
-nextSequence();
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
 
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 
-$("#green").click(function(){
+}
+
+$("#green").click(function () {
   var addAudio = new Audio("sounds/green.mp3");
   addAudio.play();
 });
 
-$("#red").click(function(){
+$("#red").click(function () {
   var addAudio = new Audio("sounds/red.mp3");
   addAudio.play();
 });
 
-$("#blue").click(function(){
+$("#blue").click(function () {
   var addAudio = new Audio("sounds/blue.mp3");
   addAudio.play();
 });
 
-$("#yellow").click(function(){
+$("#yellow").click(function () {
   var addAudio = new Audio("sounds/yellow.mp3");
   addAudio.play();
 });
 
+// let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+// audio.play();
 
 
-
-// // $("button").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+function animatePress(currentColour) {
+  $("#").addClass("pressed");
+  console.log("this is working")
+}
