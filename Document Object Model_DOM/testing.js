9100c924e4b626a109a23e8f89d91d498fc07296
Tim Kirves <tim.kirves@gmail.com>
2021-03-27 22:49:25 +0200

    testing examples about js

--- Document Object Model_DOM/testing.js
+++ Document Object Model_DOM/testing.js
@@ -1,22 +1,46 @@
 
 let greeting = prompt("Please enter number");
-let greeting2 = prompt("Please enter decimal number");
+let greeting2 = prompt("Please ented float number");
 let greeting3 = prompt("Please enter phrase")
 
 let firstInteger = 4;
 let firstDecimal = 4.0;
 let firstString = 'HackerRank ';
 
-let secondInteger = Number(greeting);
-let secondDecimal = Number(greeting2);
+let secondInteger = Math.floor(Number(greeting));
+let secondDecimal = parseFloat(greeting2);
 let secondString = greeting3;
 
-let sumInteger = firstInteger + secondInteger;
+let sumInteger = firstInteger + secondInteger;1
+
 let sumDecimal = firstDecimal + secondDecimal;
 let sumString = firstString + secondString;
 
 
-console.log(sumInteger);
-console.log(sumDecimal);
-console.log(sumString);
+console.log(sumInteger, sumDecimal, sumString);
+
+
+var num1 = 123;
+let num2 = prompt("Please enter number");
+
+num2=Math.floor(parseFloat(num2));
+
+let sumNum  = num1 + num2;
+
+console.log(sumNum);
+
+
+// did some logic testings
+
+console.log(" ");
+let randomNum = prompt("Please enter random nuumber");
+let randomNum2 = Math.floor((Math.random() * 12));
+
+console.log(randomNum2 + " is generated number");
+
+let randomMultip = (randomNum * randomNum2);
+console.log(randomMultip);
+
+
+
 
